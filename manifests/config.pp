1904c1b864d05447c8ecb413e3add5ed182ceb74
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2019-08-07 15:55:39 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,7 +13,6 @@ class confluent_kafka::config {
     'zookeeper.connect' => $::confluent_kafka::zk_string,
     'broker.id'         => $::confluent_kafka::brokers[$::fqdn],
     'log.dirs'          => join($::confluent_kafka::log_dirs, ','),
-    'inter.broker.protocol.version' => $::confluent_kafka::version,
   }
 
   $kafka_config = merge($::confluent_kafka::params::kafka_config_defaults, $::confluent_kafka::kafka_config, $tmp_config)
